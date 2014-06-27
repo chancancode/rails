0e1c841e3f66bfa9ba57e841c790359132b12c0b
Philippe Creux <pcreux@gmail.com>
2014-06-27 16:42:54 -0700

    Add support for Postgresql datatype jsonb

--- activerecord/lib/active_record/connection_adapters/postgresql/oid/json.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/oid/json.rb
@@ -29,6 +29,22 @@ module ActiveRecord
             ActiveRecord::Store::StringKeyedHashAccessor
           end
         end
+
+        class Jsonb < Json # :nodoc:
+          def type
+            :jsonb
+          end
+
+          def changed_in_place?(raw_old_value, new_value)
+            # Postgres does not preserve the insignificat whitespaces when
+            # roundtripping jsonb columns. This causes some false positives for
+            # the comparision here. Therefore, we need to parse and re-dump the
+            # raw value here to ensure the (insignificant) whitespaces are
+            # consitent with our encoder's output.
+            raw_old_value = type_cast_for_database(type_cast_from_database(raw_old_value))
+            super
+          end
+        end
       end
     end
   end
